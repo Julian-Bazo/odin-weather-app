e12eeb1f6cb1f7012170f047b502b28c5d0aeeae
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-09-07 15:43:21 -0400

    Finalize before publish
    Remove comments & update npm deploy

--- src/index.js
+++ src/index.js
@@ -6,12 +6,3 @@ import search from "./search";
 const submitLocationButton = document.querySelector(".submit-location");
 
 submitLocationButton.addEventListener("click", search);
-
-
-
-// KEY: NYCJ5G62WGR8PDVCLDXLGCWBY
-
-// Check out dynamic imports?
-// Loading component
-// Maybe integrate passing through the three day forecast
-    // Not really much more learning other than just doing obj.forecast.whatever
